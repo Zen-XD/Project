be4d4bdf4ecb8d2ea9f48d22b2077c98baa30927
Zen-XD <chreemaharjan3@gmail.com>
2026-02-16 10:32:10 +0545

    added form

--- Momo/src/components/Hero.jsx
+++ Momo/src/components/Hero.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import { FaCirclePlay } from "react-icons/fa6";
+import {
+  FaArrowLeft,
+  FaArrowRight,
+  FaClock,
+  FaFacebook,
+  FaInstagram,
+  FaTiktok,
+} from "react-icons/fa";
+import { FaCirclePlay, FaLocationDot } from "react-icons/fa6";
+import { IoIosCall } from "react-icons/io";
 
 const Hero = () => {
   return (
@@ -121,7 +129,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="flex justify-center items-center gap-24 py-20 px-40">
+      <div className="flex justify-center items-center gap-24 py-20 px-40 bg-[#FAFBFB]">
         <div className="flex flex-col gap-14 w-xl">
           <div className="flex flex-col gap-2">
             <p className="text-[40px] font-bold">
@@ -152,6 +160,167 @@ const Hero = () => {
 
         <div>
           <img src="/images/cust.png" alt="" className="w-114 h-144" />
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-14 px-39 py-16">
+        <div className="flex flex-col justify-center items-center gap-2">
+          <p className="font-bold text-[39px]">
+            Get <span className="text-[#D95103]">In Touch</span>
+          </p>
+          <p className="font-bold text-[25px] text-[#0C6967]">
+            Our Friendly team would love to hear from you
+          </p>
+        </div>
+
+        <div className="flex justify-center items-center gap-10">
+          <div className="flex flex-col justify-center items-start bg-[#0C6967] text-white text-[20px] p-8 gap-10 rounded-xl w-116">
+            <div className="flex flex-col gap-6">
+              <p className="flex justify-items-start items-center gap-2 text-[20px] font-semibold">
+                <FaLocationDot size={24} /> Our Address
+              </p>
+              <p className="italic">New Baneshwor, Kathmandu, Bagmati, Nepal</p>
+            </div>
+            <div className="flex flex-col gap-6">
+              <p className="flex justify-items-start items-center gap-2 font-semibold">
+                <IoIosCall size={24} /> Our Contacts
+              </p>
+              <div className="flex justify-center gap-14">
+                <div>
+                  <p className="font-semibold">Mobile</p>
+                  <p className="italic">980 5689789</p>
+                  <p className="italic">9841 275897</p>
+                </div>
+                <div>
+                  <p className="font-semibold">Landline</p>
+                  <p className="italic">01-4783972</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-6 mb-22">
+              <p className="flex justify-items-start items-center gap-2 text-[20px] font-semibold">
+                <FaClock size={24} />
+                Our Service Time
+              </p>
+              <div className="flex gap-14">
+                <div>
+                  <p className="font-semibold">MON - FRI</p>
+                  <p className="italic">10 am - 8 pm</p>
+                </div>
+                <div>
+                  <p className="font-semibold">SAT - SUN</p>
+                  <p className="italic">Closed</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-6">
+              <p className="italic">Get in touch in social networks</p>
+              <div className="flex gap-4">
+                <a>
+                  <FaFacebook size={32} />
+                </a>
+                <a>
+                  <FaInstagram size={32} />
+                </a>
+                <a>
+                  <FaTiktok size={32} />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <form className="flex flex-col justify-center items-center gap-12 px-6 py-10 w-146.75">
+            <div className="flex gap-2 w-full">
+              <div className="flex flex-col grow gap-2">
+                <label htmlFor="">First Name</label>
+                <input
+                  type="text"
+                  name="fname"
+                  id="fname"
+                  className="h-14 border border-[#DFE2E6] rounded-md"
+                />
+              </div>
+              <div className="flex flex-col grow gap-2">
+                <label htmlFor="">Last Name</label>
+                <input
+                  type="text"
+                  name="lname"
+                  id="lname"
+                  className="h-14 border border-[#DFE2E6] rounded-md"
+                />
+              </div>
+            </div>
+            <div className="flex gap-2 w-full">
+              <div className="flex flex-col grow gap-2">
+                <label htmlFor="">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="h-14 border border-[#DFE2E6] rounded-md"
+                />
+              </div>
+            </div>
+            <div className="flex gap-2 w-full">
+              <div className="flex flex-col grow gap-2">
+                <label htmlFor="">What can we do for you</label>
+                <select
+                  name="none"
+                  id="none"
+                  className="h-14 border border-[#DFE2E6] rounded-md px-4 text-[#98A1B0]"
+                >
+                  <option value="choose" selected>
+                    Choose
+                  </option>
+                  <option value="volvo">Volvo</option>
+                  <option value="saab">Saab</option>
+                  <option value="mercedes">Mercedes</option>
+                  <option value="audi">Audi</option>
+                </select>
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div>
+                <label htmlFor="">Message</label>
+                <div className="flex justify-center items-center h-14 border border-[#DFE2E6] p-4 rounded-l-md">
+                  <label htmlFor="">
+                    <img src="/images/npflag.png" alt="" />
+                  </label>
+                  <select
+                    name="none"
+                    id="none"
+                    className="text-[#101828] border-none outline-none"
+                  >
+                    <option value="np" selected>
+                      +977
+                    </option>
+                    <option value="afg">+93</option>
+                    <option value="ch">+86</option>
+                    <option value="fji">+679</option>
+                    <option value="kir">+868</option>
+                  </select>
+                </div>
+              </div>
+              <input
+                type="text"
+                name=""
+                id=""
+                className="h-14 border border-[#DFE2E6] rounded-r-md grow"
+              />
+            </div>
+            <div>
+              <div className="flex flex-col grow gap-2">
+                <label htmlFor="">Message</label>
+                <input
+                  type="text"
+                  name="lname"
+                  id="lname"
+                  className="h-14 border border-[#DFE2E6] rounded-md"
+                />
+              </div>
+            </div>
+            <div></div>
+          </form>
         </div>
       </div>
     </div>
